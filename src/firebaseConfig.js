e72441e48acd3472fcbe8c4d7966a9cb0c0a13e5
D-Yusuf <101116260+D-Yusuf@users.noreply.github.com>
2024-09-15 12:58:49 +0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,11 +5,7 @@ import { getAuth } from "firebase/auth";
 // Firebase configuration (replace with your actual config from Firebase Console)
 const firebaseConfig = {
     apiKey: "AI",
-    authDomain: "macro-tracker-7e6be.firebaseapp.com",
-    projectId: "macro-tracker-7e6be",
-    storageBucket: "macro-tracker-7e6be.appspot.com",
-    messagingSenderId: "461998072747",
-    appId: "1:461998072747:web:f102450f85fdf87e98a8ee"
+    authDo
   };
 
 // Initialize Firebase
